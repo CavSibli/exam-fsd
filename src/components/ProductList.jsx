936a5aa2e85361dd4379d7136211e125c4d2cba4
sibli <cav.sibligs@gmail.com>
2024-06-14 09:44:47 +0200

    architecture

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,15 @@
+import Product from "./Product";
+
+
+const ProductList = () => {
+
+
+    return (<>
+    
+        <p>ProductList</p>
+        <Product />
+    
+    </>)
+}
+
+export default ProductList;
